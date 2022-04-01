a071fddfdaae42577cbbacdc8d5390970bffbbd1
SsageParuders <qinqingqing1025@gmail.com>
2022-04-01 12:53:49 -0700

    Add Cmdline tool

---
+++ ShellTool/outputs/obj/local/arm64-v8a/objs/Kernel_hwBP_exec/src/main.o.d
@@ -0,0 +1,213 @@
+./obj/local/arm64-v8a/objs/Kernel_hwBP_exec/src/main.o: ../src/main.cpp \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstdio \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__config \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/string.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/stdint.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/memory \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/type_traits \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/typeinfo \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/exception \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstddef \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstdlib \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/version \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstdint \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/new \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/utility \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__tuple \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/initializer_list \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstring \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__debug \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/iosfwd \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/wchar.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/limits \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/iterator \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__functional_base \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/tuple \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/stdexcept \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/atomic \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__threading_support \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/chrono \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ctime \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ratio \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/climits \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/limits.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/errno.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/thread \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/functional \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/system_error \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__errc \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cerrno \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/string \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/string_view \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__string \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/algorithm \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/bit \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cwchar \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cwctype \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cctype \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ctype.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/wctype.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__mutex_base \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/sstream \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ostream \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ios \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__locale \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/mutex \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/locale.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/support/android/locale_bionic.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/streambuf \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/locale \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstdarg \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__bsd_locale_fallbacks.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/bitset \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__bit_reference \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/istream \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cinttypes \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/inttypes.h \
+  ../include/HwBreakpointManager.h ../include/cvector.h \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/vector \
+  /home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__split_buffer
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstdio:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__config:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/memory:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/type_traits:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/exception:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstddef:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/version:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstdint:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/new:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/utility:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__tuple:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstring:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__debug:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/limits:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/iterator:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/tuple:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/atomic:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__threading_support:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/chrono:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ctime:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ratio:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/climits:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/limits.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/errno.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/thread:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/functional:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/system_error:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__errc:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cerrno:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/string:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/string_view:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__string:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/algorithm:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/bit:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cwchar:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cwctype:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cctype:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/wctype.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__mutex_base:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/sstream:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ostream:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/ios:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__locale:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/mutex:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/locale.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/support/android/locale_bionic.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/streambuf:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/locale:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cstdarg:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__bsd_locale_fallbacks.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/bitset:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__bit_reference:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/istream:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/cinttypes:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/inttypes.h:
+
+../include/HwBreakpointManager.h:
+
+../include/cvector.h:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/vector:
+
+/home/ssage/Desktop/ToolChains/android-ndk-r23b/sources/cxx-stl/llvm-libc++/include/__split_buffer:
